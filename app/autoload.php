3cb56bbb41cae0a642ed4b5da634829f7b0d2bdf
ahundiak <ahundiak@spike.mshome.net>
2013-09-20 03:43:13 -0500

    Added cerad2-apptourns to vendor

--- app/autoload.php
+++ app/autoload.php
@@ -8,7 +8,7 @@ use Composer\Autoload\ClassLoader;
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
-$loader->add('Cerad',   __DIR__  . '/../../cerad2/src');
+//$loader->add('Cerad',   __DIR__  . '/../../cerad2/src');
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
