b9f68a679ec1ee5466456cf8a52eba3d0c51705a
ahundiak <ahundiak@spike.mshome.net>
2014-02-19 10:56:12 -0600

    Event Processing

--- src/Cerad/Bundle/AppBundle/Action/ProjectPerson/ProjectPersonModel.php
+++ src/Cerad/Bundle/AppBundle/Action/ProjectPerson/ProjectPersonModel.php
@@ -8,26 +8,37 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class ProjectPersonModel
 {
+    // Model Listener
     public $project;
     public $person;
+    
+    // Retrieved
     public $plan;    // Project Person Plan
     public $fed;
     
+    // Injected
     protected $dispatcher;
+    protected $personRepo;
     
     public function setDispatcher(EventDispatcherInterface $dispatcher) { $this->dispatcher = $dispatcher; }
     
+    public function __construct($personRepo)
+    {
+        $this->personRepo = $personRepo;
+    }
     public function process()
     {   
     }
     
     public function create(Request $request)
     { 
-        $this->project = $request->attributes->get('project');
-        $this->person  = $request->attributes->get('person');
-        $this->plan    = $request->attributes->get('plan');
-        $this->fed     = $request->attributes->get('fed');
+        $this->project = $project = $request->attributes->get('project');
+        $this->person  = $person  = $request->attributes->get('person');
+        
+        $this->plan    = $person->getPlan($this->project);
+        
+      //$this->fed     = $request->attributes->get('fed');
         
         return $this;
-    }    
+    }
 }
