401b623b4071d71cf1710c0caa18605bc348baca
ahundiak <ahundiak@spike.mshome.net>
2014-03-26 11:36:29 -0500

    PersonsListExport

--- src/Cerad/Bundle/AppBundle/Action/ProjectPersons/ListAdmin/PersonsListExportXLS.php
+++ src/Cerad/Bundle/AppBundle/Action/ProjectPersons/ListAdmin/PersonsListExportXLS.php
@@ -8,97 +8,68 @@ class PersonsListExportXLS extends ExcelExport
 {
     protected $phoneTransformer;
     
-    protected $lodgingNightsHeaders;
-    protected $availabilityDaysHeaders;
+    protected $lodgingMap;
+    protected $availabilityMap;
     
     public function __construct()
     {
         $this->phoneTransformer = new PhoneTransformer();
     }
-    protected function setColumnWidths($ws,$widths)
-    {
-        $col = 0;
-        foreach($widths as $width)
-        {
-            $ws->getColumnDimensionByColumn($col++)->setWidth($width);
-        }
-    }
-    protected function setRowValues($ws,$row,$values)
-    {
-        $col = 0;
-        foreach($values as $value)
-        {
-            $ws->setCellValueByColumnAndRow($col++,$row,$value);
-        }
-    }
-    protected function generateOfficialsSheet($ws,$project,$officials)
+    protected $columnWidths = array
+    (
+        'ID'           => 4,
+        'Status'       => 8,
+        'Applied Date' => 16,
+        'USSF ID'      => 18,
+        'Official'     => 24,
+        'Email'        => 24,
+        'Cell Phone'   => 14,
+        'Age'          =>  4,
+        'Badge'        =>  8,
+        'Verified'     =>  4,
+        'Notes'        => 72,
+        'Home City'    => 16,
+        'USSF State'   =>  4,
+        'LO With'  =>  8, 'TR From'   =>  8, 'TR With' =>  8,
+        'Assess'   =>  8, 'Upgrading' =>  8,
+        'Team Aff' => 10, 'Team Desc' => 10,
+        'Level'    => 14, 'LE CR' =>  6, 'LE AR' =>  6,
+    );
+    protected function generateOfficialsSheet($ws,$project,$persons)
     {
         $ws->setTitle('Officials');
         
-        $headers = array_merge(
-            array(
-                'ID','Status','Official','Email','Cell Phone','Age','Home City',
-                'USSF ID','USSF State','Badge','Verified','Assess','Upgrading',
-                'Team Aff','Team Desc',
-            ),
-            $this->lodgingNightsHeaders,
-            array('LO With','TR From','TR With'),
-            $this->availabilityDaysHeaders
+        $map1 = array(
+            'ID'           => 'id',
+            'Status'       => 'status',
+            'Applied Date' => 'appliedDate',
+            'Official'     => 'name',
+            'Email'        => 'email',
+            'Cell Phone'   => 'phone',
+            'Age'          => 'gage',
+            'Home City'    => 'city',
+            'USSF ID'      => 'ussfId',
+            'USSF State'   => 'org',
+            'Badge'        => 'badge',
+            'Verified'     => 'badgeVerified',
+            'Assess'       => 'requestAssessment',
+            'Upgrading'    => 'upgrading',
+            'Team Aff'     => 'teamClubAffilation',
+            'Team Desc'    => 'teamClubName',
         );
-        $this->writeHeaders($ws,1,$headers);
-        $row = 2;
+        $map2 = array(
+            'LO With' => 'lodgingWith',
+            'TR From' => 'travelingFrom',
+            'TR With' => 'travelingWith',
+        );
+        $map = array_merge($map1,$this->lodgingMap,$map2,$this->availabilityMap);
         
-        foreach($officials as $person)
+        $row = 1;
+        $this->setHeaders($ws,array_keys($map),$row);
+        foreach($persons as $person)
         {
-            $name        = $person->getName();
-            $address     = $person->getAddress();
-            $personFed   = $person->getFed($project->getFedRole());
-            $cert        = $personFed->getCertReferee();
-            $plan        = $person->getPlan($project->getId());
-            $basic       = $plan->getBasic();
-            
-            $values = array();
-            $values[] = $plan->getId();
-            $values[] = $plan->getStatus();
-          //$values[] = null; // $plans->getDateTimeCreated()->format('Y-m-d H:i');
-            $values[] = $name->full;
-            $values[] = $person->getEmail();
-            $values[] = $this->phoneTransformer->transform($person->getPhone());
-            
-            $gender = $person->getGender();
-            $age    = $person->getAge();
-            $gage   = $gender . $age;
-            $values[] = $gage;
-            
-            $city = $address->city . ', ' . $address->state;
-            $values[] = $city;
-            
-            $values[] = 'R' . substr($personFed->getId(),5);
-            $values[] = substr($personFed->getOrgKey(),5);
-            $values[] = $cert->getBadge();
-            $values[] = $cert->getBadgeVerified();
-            
-            $values[] = $basic['requestAssessment'];
-            $values[] = $cert->getUpgrading();
-            $values[] = $basic['teamClubAffilation'];
-            $values[] = $basic['teamClubName'];
-            
-            foreach($basic['lodgingNights'] as $value)
-            {
-                $values[] = $value;
-            }
-            $values[] = $basic['lodgingWith'];
-            $values[] = $basic['travelingFrom'];
-            $values[] = $basic['travelingWith'];
-            
-            foreach($basic['availabilityDays'] as $value)
-            {
-                $values[] = $value;
-            }
-            $this->setRowValues($ws,$row++,$values);
+            $this->setRow($ws,$map,$person,$row);
         }
-        // Done
-        return;
     }
     /* =============================================================
      * The availability
@@ -157,182 +128,146 @@ class PersonsListExportXLS extends ExcelExport
     /* ========================================================
      * Officials that have requested lodging
      */
-    protected function generateLodgingSheet($ws,$project,$officials)
+    protected function generateLodgingSheet($ws,$project,$persons)
     {
         $ws->setTitle('Lodging');
         
-        $headers = array_merge(
-            array('Status','Official','Email','Cell Phone','Age','Home City'),
-            $this->lodgingNightsHeaders,
-            array('LO With','TR From','TR With'),
-            $this->availabilityDaysHeaders
+        $map1 = array(
+            'Applied Date' => 'appliedDate',
+            'Official'     => 'name',
+            'Email'        => 'email',
+            'Cell Phone'   => 'phone',
+            'Age'          => 'gage',
+            'Home City'    => 'city',
         );
+        $map2 = array(
+            'LO With' => 'lodgingWith',
+            'TR From' => 'travelingFrom',
+            'TR With' => 'travelingWith',
+        );
+        $map = array_merge($map1,$this->lodgingMap,$map2,$this->availabilityMap);
         
-        $this->writeHeaders($ws,1,$headers);
-        $row = 2;
-        
-        foreach($officials as $person)
+        $row = 1;
+        $this->setHeaders($ws,array_keys($map),$row);
+        foreach($persons as $person)
         {
-            $address     = $person->getAddress();
-            $plan        = $person->getPlan($project->getId());
-            $basic       = $plan->getBasic();
-
             $needLodging = false;
-            foreach($basic['lodgingNights'] as $value)
+            foreach($person['lodgingNights'] as $value)
             {
                 if ($value == 'Yes') $needLodging = true;
             }
             if (!$needLodging) continue;
             
-            if (($plan->getStatus() == 'Rejected') || ($plan->getStatus() == 'Withdrew')) continue;
-            
-            $values = array();
-            $values[] = $plan->getStatus();
-            $values[] = $person->getName()->full;
-            $values[] = $person->getEmail();
-            $values[] = $this->phoneTransformer->transform($person->getPhone());
-            
-            $gender = $person->getGender();
-            $age    = $person->getAge();
-            $gage   = $gender . $age;
-            $values[] = $gage;
-            
-            $city = $address->city . ', ' . $address->state;
-            $values[] = $city;
-            
-            foreach($basic['lodgingNights'] as $value)
-            {
-                $values[] = $value;
-            }
-            $values[] = $basic['lodgingWith'];
-            $values[] = $basic['travelingFrom'];
-            $values[] = $basic['travelingWith'];
-            
-            foreach($basic['availabilityDays'] as $value)
-            {
-                $values[] = $value;
-            }
-            $this->setRowValues($ws,$row++,$values);
-            
+            $this->setRow($ws,$map,$person,$row);
         }
-        // Done
-        return;
     }
     /* ==========================================================
      * Put the notes on their own sheer
      * Formatting tends to be ugly
      */
-    protected function generateNotesSheet($ws,$project,$officials)
+    protected function generateNotesSheet($ws,$project,$persons)
     {
         $ws->setTitle('Notes');
 
         $headers = array(
             'Status','Official','Email','Cell Phone','Badge','Verified','Notes');
         
-        $this->writeHeaders($ws,1,$headers);
-        $row = 2;
+        $map = array(
+            'ID'           => 'id',
+            'Status'       => 'status',
+            'Applied Date' => 'appliedDate',
+            'Official'     => 'name',
+            'Email'        => 'email',
+            'Cell Phone'   => 'phone',
+            'Badge'        => 'badge',
+            'Verified'     => 'badgeVerified',
+            'Notes'        => 'notes',
+            'Upgrading'    => 'upgrading',
+        );
         
-        foreach($officials as $person)
+        $row = 1;
+        $this->setHeaders($ws,array_keys($map),$row);
+        foreach($persons as $person)
         {
-            $personFed   = $person->getFed($project->getFedRole());
-            $cert        = $personFed->getCertReferee();
-            $plan        = $person->getPlan($project->getId());
-            $basic       = $plan->getBasic();
+            if (!$person['notes']) continue;
             
-            $values = array();
-            $values[] = $plan->getStatus();
-            $values[] = $person->getName()->full;
-            $values[] = $person->getEmail();
-            $values[] = $this->phoneTransformer->transform($person->getPhone());
-            $values[] = $cert->getBadge();
-            $values[] = $cert->getBadgeVerified();
-            $values[] = $basic['notes'];
-            
-            $this->setRowValues($ws,$row++,$values);
-        }
-        // Done
-        return;
-    }
-    /* ===================================================================
-     * Deal with widths and such
-     */
-    protected $widths = array
-    (
-        'Status'       => 8,
-        'Applied Date' => 16,
-        'USSF ID'    => 18,
-        'Official'   => 24,
-        'Email'      => 24,
-        'Cell Phone' => 14,
-        'Age'        =>  4,
-        'Badge'      =>  8,
-        'Verified'   =>  4,
-        'Notes'      => 72,
-        'Home City'  => 16,
-        'USSF State' =>  4,
-      //'AV Fri'     =>  8,
-      //'AV Sat'     =>  8,
-      //'AV Sun'     =>  8,
-      //'LO Fri'     =>  6,
-      //'LO Sat'     =>  6,
-        'LO With'    =>  8,
-        'TR From'    =>  8,
-        'TR With'    =>  8,
-        'Assess'     =>  8,
-        'Upgrading'  =>  8,
-        'Team Aff'   => 10,
-        'Team Desc'  => 10,
-        'Level'      => 14,
-        'LE CR'      =>  6,
-        'LE AR'      =>  6,
-    );
-    protected function writeHeaders($ws,$row,$headers)
-    {
-        $col = 0;
-        foreach($headers as $header)
-        {
-            if (isset($this->widths[$header])) $width = $this->widths[$header];
-            else                               $width = 16;
-            
-            $ws->getColumnDimensionByColumn($col)->setWidth($width);
-            $ws->setCellValueByColumnAndRow($col,$row,$header);
-            $col++;
-        }
+            $this->setRow($ws,$map,$person,$row);
+        }        
     }
     /* ==========================================================
      * Main entry point
      */
     public function generate($project,$officials)
     {
-        $basic = $project->getBasic();
+        $persons = $this->processOfficials($project,$officials);
         
-        // Dynamic elements
-        $this->lodgingNightsHeaders = array();
-        foreach(array_keys($basic['lodgingNights']) as $key)
-        {
-            $name = 'LO ' . $key;
-            $this->widths[$name] = 8;
-            $this->lodgingNightsHeaders[] = $name;
-        }
-        $this->availabilityDaysHeaders = array();
-        foreach(array_keys($basic['availabilityDays']) as $key)
-        {
-            $name = 'AV ' . $key;
-            $this->widths[$name] = 6;
-            $this->availabilityDaysHeaders[] = $name;
-        }
+        $plan = $project->getPlan();
+        
         $this->ss = $ss = $this->createSpreadSheet();
         
         $si = 0;
         
-        $this->generateOfficialsSheet($ss->createSheet($si++),$project,$officials);
-      //$this->generateNotesSheet    ($ss->createSheet($si++),$project,$officials);
-      //$this->generateLodgingSheet  ($ss->createSheet($si++),$project,$officials);
+        $this->generateOfficialsSheet($ss->createSheet($si++),$project,$persons);
+        $this->generateNotesSheet    ($ss->createSheet($si++),$project,$persons);
+        $this->generateLodgingSheet  ($ss->createSheet($si++),$project,$persons);
       //$this->generateAvailSheet    ($ss->createSheet($si++),$project,$officials);
         
         // Finish up
-        $ss->setActiveSheetIndex(0);
+        $ss->setActiveSheetIndex(1);
         return $ss;
+    }
+    protected function processOfficials($project,$officialPlans)
+    {
+        $persons = array();
+        
+        foreach($officialPlans as $officialPlan)
+        {
+            $person = array();
+            
+            $person['id']     = $officialPlan->getId();
+            $person['status'] = $officialPlan->getStatus();
+            $person['appliedDate'] = $officialPlan->getCreatedOn()->format('Y-m-d H:i');
+            
+            $official = $officialPlan->getPerson();
+            $person['name'] = $official->getName()->full;
+            $person['email'] = $official->getEmail();
+            $person['phone'] = $this->phoneTransformer->transform($official->getPhone());
+            
+            $person['gage'] = $official->getGender() . $official->getAge();
+            
+            $address = $official->getAddress();
+            $person['city'] = sprintf('%s, %s', $address->city, $address->state);
+            
+            $fed  = $official->getFed($project->getFedRole());
+            $cert = $fed->getCertReferee();
+            
+            $person['ussfId']        = 'R' . substr($fed->getId(),5);
+            $person['org']           = substr($fed->getOrgKey(),5);
+            $person['badge']         = $cert->getBadge();
+            $person['badgeVerified'] = $cert->getBadgeVerified();
+            $person['upgrading']     = $cert->getUpgrading();
+            
+            $basic = $officialPlan->getBasic();
+            
+            foreach($basic['lodgingNights'] as $key => $value)
+            {
+                $header = 'LO ' . $key;
+                $index  = 'lo'  . $key;
+                $this->columnWidths[$header] = 8;
+                $this->lodgingMap[$header] = $index;
+                $person[$index] = $value;
+            }
+            foreach($basic['availabilityDays'] as $key => $value)
+            {
+                $header = 'AV ' . $key;
+                $index  = 'av'  . $key;
+                $this->columnWidths[$header] = 6;
+                $this->availabilityMap[$header] = $index;
+                $person[$index] = $value;
+            }
+            $persons[] = array_merge($person,$basic);
+        }
+        return $persons;
     }
 }
 ?>
